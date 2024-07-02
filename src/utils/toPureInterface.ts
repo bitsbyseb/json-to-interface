318c6badcf5fceba53913338898b23e68e6b505a
Johan Puentes <puentespulidojohansebastian@gmail.com>
2024-07-01 21:49:24 -0500

    .

--- src/utils/toPureInterface.ts
+++ src/utils/toPureInterface.ts
@@ -3,20 +3,23 @@ import sentenceCase from "./sentenceCase";
 
 export default function toPureInterface(input: InterfaceItem) {
     const interfaces: string[] = [];
+
+    function isAttribute(value:InterfaceItem | Attribute):value is Attribute {
+        return "type" in value;
+    }
+
     function execute(input: InterfaceItem) {
         const { attributes } = input;
 
         const asStr = attributes.map((x) => {
-            const isAttribute = Object.keys(x).includes("type");
-
-            if (isAttribute) {
-                const { name, type } = x as Attribute;
+            if (isAttribute(x)) {
+                const { name,type } = x;
                 return `\t${name}: ${type}`;
             } else {
-                const { name } = x as InterfaceItem;
+                const { name } = x;
                 interfaces.push(
                     `interface ${sentenceCase(name)} {\n${
-                        execute(x as InterfaceItem).asStr.join(",\n")
+                        execute(x).asStr.join(",\n")
                     }\n}`,
                 );
                 return `\t${name}:${sentenceCase(name)}`;
